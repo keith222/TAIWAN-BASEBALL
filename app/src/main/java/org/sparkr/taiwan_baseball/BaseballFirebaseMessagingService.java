272353109d3fdab123aec947e8bc6740f9b2f9c2
Yang Tun-Kai <ks97121@gmail.com>
2021-07-18 00:38:02 +0800

    Update remote notification feature

--- app/src/main/java/org/sparkr/taiwan_baseball/BaseballFirebaseMessagingService.java
+++ app/src/main/java/org/sparkr/taiwan_baseball/BaseballFirebaseMessagingService.java
@@ -2,6 +2,8 @@ package org.sparkr.taiwan_baseball;
 
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -13,5 +15,13 @@ public class BaseballFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Log.d("FCM", "onMessageReceived:"+remoteMessage.getFrom());
+        Log.d("FCM", "onMessageReceivedData:"+remoteMessage.getData());
+    }
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+
+        // Log.d("FCM", "FCM Token:"+s);
     }
 }
